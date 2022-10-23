fc76cd4aa30a2566226d23e32f0a8db93d7cef46
Maltilda-Nyaboke <maltilda.bosibori@student.moringaschool.com>
2022-10-23 12:25:26 +0300

    ft:create first serializer

--- dayta/serializer.py
+++ dayta/serializer.py
@@ -1,1 +1,9 @@
+from dataclasses import fields
+from pyexpat import model
 from rest_framework import serializers
+from .models import Excel
+
+class ExcelSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Excel
+        fields = ('id', 'name', 'date', 'time', 'shop','maziwa_kubwa','maziwa_ndogo','premimum','daily_hope','geocoords')
