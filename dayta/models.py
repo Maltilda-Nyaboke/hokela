a963566f757746f5e4177a29b2e270eb6cebca35
Maltilda-Nyaboke <maltilda.bosibori@student.moringaschool.com>
2022-10-23 11:48:12 +0300

    feature:create model

--- dayta/models.py
+++ dayta/models.py
@@ -1,3 +1,18 @@
+from datetime import date
+from distutils.ccompiler import show_compilers
+from xmlrpc.client import _HostType
 from django.db import models
 
 # Create your models here.
+
+class excel(models.Model):
+    id = models.AutoField(primary_key=True)
+    date = models.DateTimeField()
+    time =models.TimeField()
+    name = models.CharField(max_length =75)
+    shop = models.CharField(max_length =255)
+    maziwa_kubwa = models.IntegerField()
+    maziwa_ndogo = models.IntegerField()
+    premimum = models.IntegerField()
+    daily_hope = models.IntegerField()
+    geocoords = models.IntegerField(null=True)
